f2a1b19b9ea26b736bd1efa379a11c3776ab5e1b
sudhanshusharma99 <sudhanshusharma20002@gmail.com>
2022-07-28 18:18:55 +0530

    update procfile and app.py file

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
+import os
 
 app = Flask(__name__)
 model = pickle.load(open('hr_linearregression.pkl','rb')) 
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
 
 @app.route('/')
 def home():
